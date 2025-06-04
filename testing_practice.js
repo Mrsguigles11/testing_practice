13f2e0d0f82ef58bba76002eeeb437db65101aa9
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2025-06-04 12:29:47 +0100

    feat:Add a reverseString function and its corrresponding test

--- testing_practice.js
+++ testing_practice.js
@@ -1,6 +1,17 @@
 
-function capitalise(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
+function capitalise(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-module.exports = {capitalise};
+function reverseString(string) {
+
+    let array = [];
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        array.push(string.charAt(i));
+    }
+
+    return array.join("");
+}
+
+module.exports = {capitalise, reverseString};
--- testing_practice.test.js
+++ testing_practice.test.js
@@ -1,8 +1,14 @@
 
-const {capitalise} = require('./testing_practice.js');
+const {capitalise, reverseString} = require('./testing_practice.js');
 
 test('capitalises first letter of string', () => {
     expect(capitalise("hello")).toBe("Hello");
     expect(capitalise("hello harry")).toBe("Hello harry");
     expect(capitalise(" hello harry")).toBe(" hello harry");
 })
+
+test('reverses a string', () => {
+    expect(reverseString("hello")).toBe("olleh");
+    expect(reverseString("hello harry")).toBe("yrrah olleh");
+})
+
